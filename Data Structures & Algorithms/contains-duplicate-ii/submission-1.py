0bd6af740e5eb21d7fe946fbf9933787fd718045
Oleksandr <164345093+otorba@users.noreply.github.com>
2026-06-10 10:52:13 +0200

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+        L = 0
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
+
+
+s = Solution()
+assert s.containsNearbyDuplicate([1, 2, 3, 1], 3) is True  # health check
+assert s.containsNearbyDuplicate([1, 2, 3, 1], 2) is False  # negative
+assert s.containsNearbyDuplicate([], 1) is False  # degeneration
+assert s.containsNearbyDuplicate([1, 2, 3, 1], 0) is False  # degeneration

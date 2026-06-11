64569ef6c41cf2cd3a46f570c26f511626d3b186
Oleksandr <164345093+otorba@users.noreply.github.com>
2026-06-11 21:52:34 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = round((l + r) / 2)
+            if target < nums[m]:
+                r = m - 1
+            elif target > nums[m]:
+                l = m + 1
+            else:
+                return m
+        return -1
+
+s = Solution()
+assert s.search([1,2,3,4,5], target=5) == 4 # happy path
+assert s.search([1,2,3,4,5], target=7) == -1 # negative path
+assert s.search([], target=7) == -1 # degenerative path

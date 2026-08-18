09217aec1a5fe21fd52ffd4ca37b1745796d7314
Oleksandr <164345093+otorba@users.noreply.github.com>
2026-08-18 20:40:59 +0200

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-1

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-1.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MinimumRecolors(string blocks, int k) {
+        var l = 0;
+        var r = 0;
+        var min = int.MaxValue;
+        var occupiedNumber = 0;
+
+        while (r < blocks.Length) {
+            if (blocks[r] == 'W')
+                occupiedNumber++;
+
+            while (r - l + 1 > k) {
+                if (blocks[l] == 'W')
+                    occupiedNumber--;
+                l++;
+            }
+
+            if (r - l + 1 == k)
+                min = Math.Min(min, occupiedNumber);
+
+            r++;
+        }
+
+        return min;
+    }
+}
